25f9e513025aa94843cb4c1b29fbfe463437412b
Rudy Quiroga <rudyquirogag@gmail.com>
2023-03-10 19:02:10 -0400

    ADD: Agrega conexion a mongodb

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from .routers import users
 from motor.motor_asyncio import AsyncIOMotorClient
 
+load_dotenv()
 app = FastAPI()
 
-MONGODB_URL = os.environ["MONGODB_URL"]
+MONGODB_URI = os.environ["MONGODB_URI"]
 
-client = AsyncIOMotorClient(MONGODB_URL)
+client = AsyncIOMotorClient(MONGODB_URI)
 
 app.include_router(users.router)
